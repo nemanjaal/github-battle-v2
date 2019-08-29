4b6ae13a246ae7f4c14f7287da7bbf24f544e6c3
Nemanja Aleksic <nemanjaaleksic1991@gmail.com>
2019-08-29 22:49:22 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
+import Battle from './components/Battle';
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+    render() {
+        return (
+        <div className='container'>
+            <Battle />
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    // React Element,
+    <App />,
+    // Where to render to the Element to
+    document.getElementById('app')
+)
